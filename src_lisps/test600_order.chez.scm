e69fae621786d505a8fe8ae7ec1502027be0fab9
kakadu <kakadu.hafanana@gmail.com>
2017-04-25 21:56:30 +0300

    WIP on m inimal demo

--- src_lisps/test600_order.chez.scm
+++ src_lisps/test600_order.chez.scm
@@ -50,28 +50,53 @@
 ;))
 
 (list-display (run 2 (exp)
-    (conde
-      ((lambdag@ (st)
-         (begin
-            (let ((scope (subst-scope (state-S st))))
-              (let ( (t (var 't scope)) )
-                 (inc (begin
-                   (bind* ((=== exp 1 ) st) ))))))))
-
-      ((lambdag@ (st)
-         (begin
+  (lambdag@ (st)
+    (lambda ()
+     (let ((st (state-with-scope st (new-scope))))
+      (mplus
+        ; Bind was here
+        (let ((scope (subst-scope (state-S st))))
+          (let ( (t (var 't scope)) )
+             (inc
+               ((=== exp 1 ) st) )))
+        (inc
+          (mplus
+            ; Bind was here
             (let ((scope (subst-scope (state-S st))))
               (let ( (es (var 'es scope)) )
-                 (inc (begin
-                   (bind* ((=== exp 2) st) ))))))))
-
-      ((lambdag@ (st)
-         (begin
+                 (inc
+                   ((=== exp 2) st) )))
+          (inc
+            ; Bind was here
             (let ((scope (subst-scope (state-S st))))
               (let ( (zzzzzzzzzzs (var 'zzzzzzzzzs scope)) )
-                 (inc (begin
-                   (bind* ((=== exp 3) st) ))))))))
-
-
-    )
+                 (inc
+                   ((=== exp 3) st) )))))))
+  )))
 ))
+
+
+;(list-display (run 2 (exp)
+;    (conde
+;      ((lambdag@ (st)
+;            (let ((scope (subst-scope (state-S st))))
+;              (let ( (t (var 't scope)) )
+;                 (inc
+;                   ((=== exp 1 ) st) )))))
+;
+;      ((lambdag@ (st)
+;            (let ((scope (subst-scope (state-S st))))
+;              (let ( (es (var 'es scope)) )
+;                 (inc
+;                   ((=== exp 2) st) )))))
+;
+;      ((lambdag@ (st)
+;
+;            (let ((scope (subst-scope (state-S st))))
+;              (let ( (zzzzzzzzzzs (var 'zzzzzzzzzs scope)) )
+;                 (inc
+;                   ((=== exp 3) st) )))))
+;
+;
+;    )
+;))
