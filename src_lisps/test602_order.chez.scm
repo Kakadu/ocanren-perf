e2bf1fc9d81da457a3d7d0e2a9cdab8742a51fcb
kakadu <kakadu.hafanana@gmail.com>
2017-04-28 17:14:36 +0300

    This is test where everything is wrong

--- src_lisps/test602_order.chez.scm
+++ src_lisps/test602_order.chez.scm
@@ -3,95 +3,130 @@
 (include "list-display.scm")
 
 
-;(define evalo (lambda (m n)
-;  (conde
-;    ((fresh (f f2)
-;        (conde
-;          ;((fresh (x)
-;          ;    (=== f2 1)
-;          ;))
-;          ((lambdag@ (st)
-;              (let ( (x (var 'x (subst-scope (state-S st)))) )
-;               (printf "create inc in fresh ==== ~a\n" (list 'x))
-;               (inc (begin
-;                     (printf "inc in fresh forced: ~a \n" (list 'x))
-;                     ((=== f2 1) st) )))))
-;          ;((fresh (p)
-;          ;    (=== f2 2)
-;          ;))
-;          ((lambdag@ (st)
-;              (let ( (p (var 'p (subst-scope (state-S st)))) )
-;               (printf "create inc in fresh ==== ~a\n" (list 'p))
-;               (inc (begin
-;                     (printf "inc in fresh forced: ~a \n" (list 'p))
-;                     ((=== f2 2) st) )))))
-;        )
-;        (evalo f f2)
-;    ))
-;  )
-;))
+(define evalo (lambda (m)
+  (begin (printf " applying evalo to m\n")
+  (fresh (f2)
+      (conde
+        ((fresh (x)
+            (=== f2 1)
+        ))
+        ;((lambdag@ (st)
+        ;    (let ( (x (var 'x (subst-scope (state-S st)))) )
+        ;     (printf "create inc in fresh ==== ~a\n" (list 'x))
+        ;     (inc (begin
+        ;           (printf "inc in fresh forced: ~a \n" (list 'x))
+        ;           ((=== f2 1) st) )))))
+        ((fresh (p)
+            (=== f2 2)
+        ))
 
+        ;((lambdag@ (st)
+        ;    (let ( (p (var 'p (subst-scope (state-S st)))) )
+        ;     (printf "create inc in fresh ==== ~a\n" (list 'p))
+        ;     (inc (begin
+        ;           (printf "inc in fresh forced: ~a \n" (list 'p))
+        ;           ((=== f2 2) st) )))))
+      )
+      (evalo 4)
+  )
+)))
 
-(define evalo (lambda (m n)
-    (fresh (f f2)
-        ;(conde
-        (lambdag@ (st)
-           (begin
-             (printf " created inc in conde\n")
-             (inc
-              (let ((st (state-with-scope st (new-scope))))
-                 (printf " force a conde\n")
-                 (mplus*
-                   ((lambdag@ (st)
-                       (let ( (x (var 'x (subst-scope (state-S st)))) )
-                        (printf "create inc in fresh ==== ~a\n" (list 'x))
-                        (inc (begin
-                              (printf "inc in fresh forced: ~a \n" (list 'x))
-                              ((=== f2 1) st) )))) st)
-                    ((lambdag@ (st)
-                        (let ( (p (var 'p (subst-scope (state-S st)))) )
-                         (printf "create inc in fresh ==== ~a\n" (list 'p))
-                         (inc (begin
-                               (printf "inc in fresh forced: ~a \n" (list 'p))
-                               ((=== f2 2) st) )))) st)
-                ))))
-
-        )
-        (evalo f f2)
-    )
-))
-
-;(define evalo (lambda (m n)
-;    (fresh (f f2)
-;        ;(conde
-;        (lambdag@ (st)
-;           (begin
-;             (printf " created inc in conde\n")
-;             (inc
-;              (let ((st (state-with-scope st (new-scope))))
-;                 (printf " force a conde\n")
-;                 (mplus*
-;                   ((lambdag@ (st)
-;                       (let ( (x (var 'x (subst-scope (state-S st)))) )
-;                        (printf "create inc in fresh ==== ~a\n" (list 'x))
-;                        (inc (begin
-;                              (printf "inc in fresh forced: ~a \n" (list 'x))
-;                              ((=== f2 1) st) )))) st)
-;                    ((lambdag@ (st)
-;                        (let ( (p (var 'p (subst-scope (state-S st)))) )
-;                         (printf "create inc in fresh ==== ~a\n" (list 'p))
-;                         (inc (begin
-;                               (printf "inc in fresh forced: ~a \n" (list 'p))
-;                               ((=== f2 2) st) )))) st)
-;                ))))
+;(define evalo (lambda (m)
+;  (begin (printf " applying evalo to m\n")
+;  (fresh (f2)
+;      ;(conde
+;      (lambdag@ (st)
+;         (begin
+;           (printf " created inc in conde\n")
+;           (inc
+;            (let ((st (state-with-scope st (new-scope))))
+;               (printf " force a conde\n")
+;               (mplus*
+;                    (let ( (x (var 'x (subst-scope (state-S st)))) )
+;                          (printf "create inc in fresh ==== ~a\n" (list 'x))
+;                          (inc (begin
+;                                (printf "inc in fresh forced: ~a \n" (list 'x))
+;                                ((=== f2 1) st) )))
 ;
-;        )
-;        (evalo f f2)
-;    )
-;))
+;                    (let ( (p (var 'p (subst-scope (state-S st)))) )
+;                     (printf "create inc in fresh ==== ~a\n" (list 'p))
+;                     (inc (begin
+;                           (printf "inc in fresh forced: ~a \n" (list 'p))
+;                           ((=== f2 2) st) ))))
+;                      ))))
+;      (evalo 4)
+;  )
+;)))
+
+;(define evalo (lambda (m)
+;  (begin (printf " applying evalo to m\n")
+;  (fresh (f2)
+;      (lambdag@ (st)
+;        (begin
+;          (printf " created inc in conde\n")
+;          (inc
+;            (let ((st (state-with-scope st (new-scope))))
+;                (printf " force a conde\n")
+;                (mplus
+;                  (let ( (x (var 'x (subst-scope (state-S st)))) )
+;                          (printf "create inc in fresh ==== ~a\n" (list 'x))
+;                          (inc (begin
+;                                (printf "inc in fresh forced: ~a \n" (list 'x))
+;                                ((=== f2 1) st) )))
+;                  (inc (begin
+;                          (printf " force inc from mplus*\n")
+;                          (let ( (p (var 'p (subst-scope (state-S st)))) )
+;                             (printf "create inc in fresh ==== ~a\n" (list 'p))
+;                             (inc (begin
+;                                   (printf "inc in fresh forced: ~a \n" (list 'p))
+;                                   ((=== f2 2) st) ))))
+;                  )
+;                )
+;              )
+;            ) ))
+;      (evalo 4)
+;  )
+;)))
+
+;(define evalo (lambda (m)
+;  (begin (printf " applying evalo to m\n")
+;  (lambdag@ (st)
+;    (let ((scope (subst-scope (state-S st))))
+;      (let ((f2 (var 'f2 scope)) )
+;        (printf "create inc in fresh ==== ~a\n" (list 'f2 ) )
+;        (inc (begin
+;            (printf "inc in fresh forced: ~a \n" (list 'f2 ))
+;            (bind
+;              (begin (printf " created inc in conde\n")
+;                (inc
+;                  (let ((st (state-with-scope st (new-scope))))
+;                    (printf " force a conde\n")
+;                    (mplus
+;                      (let ( (x (var 'x (subst-scope (state-S st)))) )
+;                               (printf "create inc in fresh ==== ~a\n" (list 'x))
+;                               (inc (begin
+;                                     (printf "inc in fresh forced: ~a \n" (list 'x))
+;                                     ((=== f2 1) st) )))
+;                      (inc (begin
+;                        (printf " force inc from mplus*\n")
+;                        (let ( (p (var 'p (subst-scope (state-S st)))) )
+;                            (printf "create inc in fresh ==== ~a\n" (list 'p))
+;                            (inc (begin
+;                                  (printf "inc in fresh forced: ~a \n" (list 'p))
+;                                  ((=== f2 2) st) ))))
+;                      )
+;                    )
+;                  )
+;                )
+;              )
+;              (evalo 4) )
+;            ))
+;        )))
+;)))
+
 
 (list-display
-  (myrun2 1 (q r)
-    (evalo q r)
+  (myrun1 1 (q)
+    (evalo q)
   )
 )
