c6a811aff15d83b6e04384baf7e503151c44c7ac
Kakadu <Kakadu@pm.me>
2022-04-19 19:32:47 +0300

    Polishing benchmarks for ocanren01
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- ocanren01/Quine_decls.ml
+++ ocanren01/Quine_decls.ml
@@ -6,6 +6,61 @@
 open Printf
 open OCanren
 
+module StringLo = struct
+  type ground = GT.string [@@deriving gt ~options:{ show; fmt; gmap }]
+  type logic = string OCanren.logic
+
+  let logic =
+    { GT.gcata = ()
+    ; fix = (fun _ _ -> assert false)
+    ; plugins =
+        object
+          method fmt = GT.fmt OCanren.logic (fun ppf -> Format.fprintf ppf "%s")
+          method gmap x = [%gmap: GT.string OCanren.logic] () x
+        end
+    }
+  ;;
+
+  type injected = GT.string OCanren.ilogic
+
+  let prj_exn = OCanren.prj_exn
+  let reify = OCanren.reify
+end
+
+module ListLo = struct
+  type 'a ground = 'a Std.List.ground [@@deriving gt ~options:{ gmap; fmt }]
+  type 'a logic = 'a Std.List.logic
+
+  let logic =
+    { Std.List.logic with
+      plugins =
+        object
+          method fmt fa ppf xs =
+            let default ppf xs = (GT.fmt Std.List.logic) fa ppf xs in
+            match xs with
+            | Var _ -> default ppf xs
+            | Value _ ->
+              let rec iter ppf xs =
+                match xs with
+                | Value Std.List.Nil -> ()
+                | Value (Std.List.Cons (h, tl)) -> Format.fprintf ppf "%a %a" fa h iter tl
+                | Var _ -> Format.fprintf ppf " . %a" default xs
+              in
+              Format.fprintf ppf "(%a)" iter xs
+
+          method gmap fa xs = [%gmap: 'a Std.List.logic] (GT.lift fa) () xs
+        end
+    }
+  ;;
+
+  type 'a injected = 'a Std.List.groundi
+
+  let prj_exn = Std.List.prj_exn
+  let reify = Std.List.reify
+end
+
+(* let (_ : int) = GT.gmap ListLo.logic *)
+
 module Std = struct
   include Std
 
@@ -71,9 +126,9 @@ module Gterm = struct
   type nonrec ('s, 'xs) t =
     | Symb of 's
     | Seq of 'xs
-  [@@deriving gt ~options:{ show; fmt; gmap }]
+  [@@deriving gt ~options:{ fmt; gmap }]
 
-  type ground = (GT.string, ground Std.List.ground) t]
+  type ground = (StringLo.ground, ground ListLo.ground) t]
 
   let t =
     { t with
@@ -92,32 +147,18 @@ module Gterm = struct
                   method! c_Seq fmt _ xs = Format.fprintf fmt "(seq %a)" fb xs
                 end)
               fmt
-
-          method show fa fb () =
-            GT.transform
-              t
-              (fun fself ->
-                object
-                  inherit ['a, 'b, _] show_t_t fa fb fself
-                  method! c_Symb _ _ str = sprintf "(symb '%s)" (fa () str)
-                  method! c_Seq _ _ xs = sprintf "(seq %s)" (fb () xs)
-                end)
-              ()
         end
     }
   ;;
 
-  type rterm = ground [@@deriving gt ~options:{ show; fmt; gmap }]
-
-  let ground = rterm (* Hack to apply custom transformations for type t *)
-
-  type lterm = (GT.string OCanren.logic, lterm Std.List.logic) t OCanren.logic
-  [@@deriving gt ~options:{ fmt }]
+  (* This is a hack to apply custom printers for logic strings and lists *)
+  type logic = (StringLo.logic, logic ListLo.logic) t OCanren.logic
+  [@@deriving gt ~options:{ fmt; gmap }]
 
   type injected = (GT.string OCanren.ilogic, injected Std.List.groundi) t ilogic
 
-  let show_rterm = Format.asprintf "%a" (GT.fmt rterm)
-  let show_lterm = Format.asprintf "%a" (GT.fmt lterm)
+  let show_rterm = Format.asprintf "%a" (GT.fmt ground)
+  let show_lterm = Format.asprintf "%a" (GT.fmt logic)
 end
 
 let gterm_reifier = Gterm.reify
@@ -127,121 +168,37 @@ module Gresult = struct
   type nonrec ('s, 't, 'xs) t =
     | Closure of 's * 't * 'xs
     | Val_ of 't
-  [@@deriving gt ~options:{ show; fmt; gmap }]
+  [@@deriving gt ~options:{ fmt; gmap }]
 
   type ground =
-    (GT.string, Gterm.ground, (GT.string, ground) Std.Pair.ground Std.List.ground) t]
+    ( StringLo.ground
+    , Gterm.ground
+    , (StringLo.ground, ground) Std.Pair.ground Std.List.ground )
+    t]
 
-  (*
-  module X = struct
-    type ('s, 't, 'xs) t =
-      | Closure of 's * 't * 'xs
-      | Val of 't
-    [@@deriving gt ~options:{ show; fmt; gmap }]
-
-    let fmap f g h = function
-      | Closure (a, b, c) -> Closure (f a, g b, h c)
-      | Val b -> Val (g b)
-    ;;
-
-    (* let t = {t with
-      gcata = ();
-      plugins = object
-        method gmap = fmap (* t.plugins#gmap *)
-        method show = t.plugins#show
-           (* GT.transform(t)
-              (GT.lift fa) (GT.lift fb)
-              (object inherit ['a,'b] show_t
-                method c_Clos _ _ a b c  =
-                  sprintf "(closure %s %s %s)" (a.GT.fx ()) (b.GT.fx ()) (c.GT.fx ())
-                method c_Val  _ _ t =
-                  sprintf "(val %s)" (t.GT.fx ())
-               end)
-              ()
-              bx *)
-       end
-    } *)
-  end
-*)
-  (* include Fmap3 (X)
-
-  type rresult = (GT.string, Gterm.rterm, (GT.string * rresult) Std.List.ground) X.t
-  [@@deriving gt ~options:{ fmt }]
-
-  type lresult =
-    ( GT.string logic
-    , Gterm.lterm
-    , (GT.string logic, lresult) Std.Pair.logic Std.List.logic )
-    X.t
-    logic
-  [@@deriving gt ~options:{ fmt }]
-
-  type fresult = (rresult, lresult) injected *)
-
-  (* let closure s t xs = closure (s, t, xs) *)
-
-  (* let val_ t = inj @@ Val_ t *)
   let show_string = GT.(show string)
   let show_stringl = GT.(show OCanren.logic) show_string
   let rec show_rresult r = Format.asprintf "%a" (GT.fmt ground) r
   let show_lresult (r : logic) = Format.asprintf "%a" (GT.fmt logic) r
 end
 
-(* let (_ : int) = Gresult.closure *)
 let gresult_reifier = Gresult.reify
-(* let rec gresult_reifier c : Gresult.fresult -> Gresult.lresult =
-  Gresult.reify
-    OCanren.reify
-    gterm_reifier
-    (Std.List.reify (Std.Pair.reify OCanren.reify gresult_reifier))
-    c
-;; *)
-
 let ( !! ) x = inj x
 
 open Gterm
 open Gresult
 
 type lenv = (GT.string OCanren.logic, Gresult.logic) Std.Pair.logic Std.List.logic
-[@@deriving gt ~options:{ show }]
+[@@deriving gt ~options:{ fmt }]
 
-(* type fenv = ((string, rresult) Std.Pair.ground Std.List.ground, lenv) injected *)
 type fenv = (string OCanren.ilogic, Gresult.injected) Std.Pair.groundi Std.List.groundi
 
-let reif_env : (_ (* ??? *), lenv) Reifier.t =
+let reif_env : (_, lenv) Reifier.t =
   Std.List.reify (Std.Pair.reify OCanren.reify gresult_reifier)
 ;;
 
-let show_lenv : lenv -> string = GT.show lenv
-
-(* let show_reif_env h e =
-  GT.(
-    show Std.List.logic @@ show Std.Pair.logic (show logic (GT.lift Fun.id)) show_lresult)
-  @@ (* (fun _ _ -> assert false) *)
-     (* (Std.List.reify @@ Std.Pair.reify OCanren.reify gresult_reifier) *)
-  reif_env h e
-;; *)
-
-(* let (_:int) = GT.(show Std.List.logic @@
-      show Std.Pair.logic (show logic @@ (fun s -> s)) show_lresult)
-let (_: _ -> fenv -> _) = show_reif_env *)
-
-(* let unienv ?loc = unitrace ?loc @@ show_reif_env *)
-
 let show_reif_term h t = show_lterm @@ gterm_reifier h t
 let show_reif_result h t = show_lresult @@ gresult_reifier h t
-
-(* let uniresult ?loc = unitrace ?loc @@ show_reif_result
-let uniterm ?loc = unitrace ?loc @@ show_reif_term *)
-(* let uni_term_list ?loc =
-  unitrace ?loc
-    (fun h t -> GT.(fun t -> show List.logic Gterm.show_lterm t) @@
-      (List.reify gterm_reifier h t)
-    ) *)
-
-(* let show_reif_string h t = GT.(show logic @@ show string) @@ OCanren.reify h t *)
-(* let unistring ?loc = unitrace ?loc @@ show_reif_string *)
-
 let ( =/= ) = OCanren.( =/= )
 let ( =//= ) = ( =/= )
 let ( === ) = OCanren.( === )
@@ -250,10 +207,6 @@ let ( ===!! ) = ( === )
 
 let rec lookupo x env t =
   let open OCanren.Std in
-  (* let (=/=) = diseqtrace show_reif_string in
-  let (===) ?loc = unienv ?loc in
-  let (===!) ?loc = unistring ?loc in
-  let (===!!) ?loc = uniresult ?loc in *)
   fresh
     (rest y v)
     (Std.Pair.pair y v % rest === env)
@@ -261,10 +214,7 @@ let rec lookupo x env t =
 ;;
 
 let rec not_in_envo x env =
-  (* let (=/=) = diseqtrace show_reif_string in
-  let (===) ?loc = unienv ?loc in *)
   let open OCanren.Std in
-  (* printfn "entering not_in_envo"; *)
   conde
     [ fresh (y v rest) (env === Std.pair y v % rest) (y =/= x) (not_in_envo x rest)
     ; nil () === env
@@ -312,48 +262,8 @@ and evalo (term : Gterm.injected) (env : fenv) (r : Gresult.injected) =
     ]
 ;;
 
-let ( ~~ ) s = symb @@ inj s
 let s tl = seq (Std.List.list tl)
 let nil = Std.nil ()
-
-(* let run_term (text,t) = printf "> %s\n%!%s\n\n%!" text @@
-  run q (fun q -> evalo t nil (val_ q)) (fun qs ->
-      if Stream.is_empty qs
-      then "fail"
-      else (Stream.hd qs)#refine gterm_reifier ~inj:Gterm.to_logic |> show_lterm
-    ) *)
-(*
-let quine_c =
-  s[s[~~"lambda"; s[~~"x"];
-      s[~~"list"; ~~"x"; s[~~"list"; s[~~"quote"; ~~"quote"]; ~~"x"]]];
-    s[~~"quote";
-      s[~~"lambda"; s[~~"x"];
-        s[~~"list"; ~~"x"; s[~~"list"; s[~~"quote"; ~~"quote"]; ~~"x"]]]]]
-
-let _f () =
-  printf "Evaluate:\n\n%!";
-  run_term (REPR( ~~"x" ));
-  run_term (REPR( (s[s[~~"quote"; ~~"x"]; s[~~"quote"; ~~"y"]]) ));
-  run_term (REPR( s[~~"quote"; ~~"x"; ~~"y"] ));
-  run_term (REPR( s[~~"quote"; ~~"x"] ));
-  run_term (REPR( s[~~"list"] ));
-  run_term (REPR( s[~~"list"; s[~~"quote"; ~~"x"]; s[~~"quote"; ~~"y"]] ));
-  run_term (REPR( s[s[~~"lambda"; s[~~"x"]; ~~"x"]; s[~~"list"]]        ));
-  run_term (REPR( s[ s[ s[~~"lambda"; s[~~"x"]; s[~~"lambda"; s[~~"y"]; s[~~"list"; ~~"x"; ~~"y"]]]; s[~~"quote"; ~~"1"]];
-                     s[ ~~"quote"; ~~"2"]] ));
-  run_term (REPR( s[s[~~"lambda"; s[~~"lambda"]; s[~~"lambda"; s[~~"list"]]]; s[~~"lambda"; s[~~"x"]; ~~"x"]] ));
-  run_term (REPR( s[~~"quote"; ~~"list"] ));
-  run_term (REPR( quine_c ));
-  ()
-;;
-
-let gen_terms n r = printf "> %s\n" (show_term r);
-  run q (fun q -> evalo q nil (val_ r))
-    (fun qs -> List.iter (fun t -> printf "%s\n" @@ show_term t) @@
-      Stream.take ~n:n qs);
-  Printf.printf "\n"
-*)
-
 let quineso q = evalo q nil (val_ q)
 let twineso q p = q =/= p &&& evalo q nil (val_ p) &&& evalo p nil (val_ q)
 
@@ -406,30 +316,3 @@ let find_thrines ~verbose n =
            let () = wrap3terms a in
            print_newline ()))
 ;;
-
-(*
-let _ =
-  Printf.printf "Evaluate:\n\n%!";
-  run_term @@ ~~"x";
-  run_term @@ s[s[~~"quote"; ~~"x"]; s[~~"quote"; ~~"y"]];
-  run_term @@ s[~~"quote"; ~~"x"; ~~"y"];
-  run_term @@ s[~~"quote"; ~~"x"];
-  run_term @@ s[~~"list"];
-  run_term @@ s[~~"list"; s[~~"quote"; ~~"x"]; s[~~"quote"; ~~"y"]];
-  run_term @@ s[s[~~"lambda"; s[~~"x"]; ~~"x"]; s[~~"list"]];
-  run_term @@ s[s[s[~~"lambda"; s[~~"x"]; s[~~"lambda"; s[~~"y"]; s[~~"list"; ~~"x"; ~~"y"]]]; s[~~"quote"; ~~"1"]]; s[~~"quote"; ~~"2"]];
-  run_term @@ s[s[~~"lambda"; s[~~"lambda"]; s[~~"lambda"; s[~~"list"]]]; s[~~"lambda"; s[~~"x"]; ~~"x"]];
-  run_term @@ s[~~"quote"; ~~"list"];
-  run_term @@ quine_c;
-
-  Printf.printf "%!Generate:\n\n%!";
-  gen_terms 5 @@ ~~"x";
-  gen_terms 5 @@ s[];
-  gen_terms 5 @@ s[~~"lambda"; s[~~"x"]; s[~~"x"; ~~"y"; ~~"z"]];
-
-  Printf.printf "%!Quines:\n\n%!";
-  find_quines 5;
-
-  Printf.printf "%!Twines:\n\n%!";
-  find_twines ()
-  *)
--- ocanren01/test008_appendo.ml
+++ ocanren01/test008_appendo.ml
@@ -1,11 +1,39 @@
 open OCanren
 open Tester
-open Quine_decls
 
 let nels = 20
-let input_ints = Array.init nels (fun i -> 1 + (i mod 20)) |> Array.to_list |> List.map (fun x -> inj@@lift x)
+
+let input_ints =
+  Array.init nels (fun i -> 1 + (i mod 20)) |> Array.to_list |> List.map (fun x -> inj x)
+;;
+
 let () = assert (nels == List.length input_ints)
 
-let show_int_list = GT.(show Std.List.ground @@ show int)
+let rec appendo a b ab =
+  let open Std in
+  conde
+    [ a === nil () &&& (b === ab)
+    ; fresh (h t ab') (a === h % t) (h % ab' === ab) (appendo t b ab')
+    ]
+;;
+
+let show_int_list = [%show: GT.int OCanren.logic Std.List.logic] ()
+let run_list n = run_r (Std.List.reify OCanren.reify) show_int_list n
+
+let __ () =
+  TimeHelper.wrap (fun ~verbose ->
+      run_list
+        (-1)
+        qr
+        qrh
+        (REPR (fun q r -> Std.List.appendo (Std.List.list [ !!1; !!2; !!3 ]) q r)))
+;;
+
 let () =
-  run_exn show_int_list  (-1)  qr qrh (REPR (fun q r -> Std.List.appendo q r (Std.List.list input_ints) ))
+  TimeHelper.wrap (fun ~verbose ->
+      run_list
+        (-1)
+        qr
+        qrh
+        (REPR (fun q r -> Std.List.appendo q r (Std.List.list input_ints))))
+;;
